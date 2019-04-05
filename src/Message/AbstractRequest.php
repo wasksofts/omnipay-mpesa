4e3a3d133825acd4dafd4ea05d51390435484d96
wasksofts <stevenkamanu@gmail.com>
2019-04-05 14:59:41 +0300

    Add files via upload

---
+++ src/Message/AbstractRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Omnipay\Mpesa\Message;
+
+use Omnipay\Common\Message\AbstractRequest as BaseAbstractRequest;
+
+/**
+ * Abstract Request
+ *
+ */
+abstract class AbstractRequest extends BaseAbstractRequest
+{
+    protected $liveEndpoint = 'https://api.example.com';
+    protected $testEndpoint = 'https://api-test.example.com';
+
+    public function getKey()
+    {
+        return $this->getParameter('key');
+    }
+
+    public function setKey($value)
+    {
+        return $this->setParameter('key', $value);
+    }
+
+    public function sendData($data)
+    {
+        $url = $this->getEndpoint().'?'.http_build_query($data, '', '&');
+        $response = $this->httpClient->get($url);
+
+        $data = json_decode($response->getBody(), true);
+
+        return $this->createResponse($data);
+    }
+
+    protected function getBaseData()
+    {
+        return [
+            'transaction_id' => $this->getTransactionId(),
+            'expire_date' => $this->getCard()->getExpiryDate('mY'),
+            'start_date' => $this->getCard()->getStartDate('mY'),
+        ];
+    }
+
+    protected function getEndpoint()
+    {
+        return $this->getTestMode() ? $this->testEndpoint : $this->liveEndpoint;
+    }
+
+    protected function createResponse($data)
+    {
+        return $this->response = new Response($this, $data);
+    }
+}
---
+++ src/Message/AuthorizeRequest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Omnipay\Mpesa\Message;
+/**
+ * Authorize Request
+ *
+ * @method Response send()
+ */
+class AuthorizeRequest extends AbstractRequest
+{
+    public function getData()
+    {
+        $this->validate('amount', 'card');
+        $this->getCard()->validate();
+        $data = $this->getBaseData();
+        return $data;
+    }
+}
